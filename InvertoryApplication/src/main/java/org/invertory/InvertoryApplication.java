55f6fb23d34c9d033cbaa8476f1ea0e3a1c6f84d
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-11-15 12:43:35 +0200

    setup springboot project

---
+++ InvertoryApplication/src/main/java/org/invertory/InvertoryApplication.java
@@ -0,0 +1,12 @@
+package org.invertory;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvertoryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InvertoryApplication.class, args);
+    }
+}
--- InvertoryApplication/src/main/java/org/invertory/Main.java
+++
@@ -1,7 +0,0 @@
-package org.invertory;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
