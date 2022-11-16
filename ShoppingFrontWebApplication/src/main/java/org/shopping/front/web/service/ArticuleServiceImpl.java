2de6cf2ba4b06892d205fd167856cd6ba1244444
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-11-16 12:54:15 +0200

    succesfull call to invertory microservice

--- ShoppingFrontWebApplication/src/main/java/org/shopping/front/web/service/ArticuleServiceImpl.java
+++ ShoppingFrontWebApplication/src/main/java/org/shopping/front/web/service/ArticuleServiceImpl.java
@@ -1,14 +1,27 @@
 package org.shopping.front.web.service;
 
-import org.apache.http.HttpStatus;
+import com.invertory.ApiException;
+import com.invertory.api.InvertoryControllerApi;
+import com.invertory.model.Invertory;
 import org.shopping.front.web.model.Articule;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ArticuleServiceImpl implements ArticuleService {
+
+    @Autowired
+    InvertoryControllerApi invertoryControllerApi;
+
     @Override
     public String createArticule(Articule articule) {
-        //TODO implement later
-        return String.valueOf(HttpStatus.SC_OK);
+
+        try {
+            final String invertoryUsingPOST = invertoryControllerApi.createInvertoryUsingPOST(new Invertory());
+
+            return invertoryUsingPOST;
+        } catch (ApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
