63c922afc8cc28df655f61de4df465e97272ff8c
Asen Nikolaev <asen.nikolaev95@abv.bg>
2022-11-28 12:57:17 +0200

    enable SSL for invertory service

--- InvertoryApplication/README.md
+++ InvertoryApplication/README.md
@@ -12,3 +12,9 @@ Building steps:
 Swagger 
 
 http://localhost:8442/swagger-ui/index.html
+
+
+SSL
+
+
+keytool -genkeypair -alias invertory-service-keystore -keyalg RSA -keysize 2048 -storetype PKCS12 -keystore invertory.p12 -validity 3650
--- InvertoryApplication/src/main/java/org/invertory/InvertoryApplication.java
+++ InvertoryApplication/src/main/java/org/invertory/InvertoryApplication.java
@@ -3,12 +3,13 @@ package org.invertory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableWebMvc
 @EnableSwagger2
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class InvertoryApplication {
     public static void main(String[] args) {
         SpringApplication.run(InvertoryApplication.class, args);
